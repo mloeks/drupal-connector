5e7e05ee11086e265665976b662458391fccd3f4
Matthias Loeks <matthias-johannes.loeks@xarvio.com>
2025-01-09 08:58:51 +0100

    Add failing test for nested attributes

--- src/test/java/com/mloeks/drupalconnector/DrupalConnectorTest.java
+++ src/test/java/com/mloeks/drupalconnector/DrupalConnectorTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -60,15 +61,51 @@ public class DrupalConnectorTest {
                         tuple("node--article", UUID.fromString("14650b8c-2af3-4e77-88f3-ea6a57e04a89"))
                 );
 
-        assertThat(candidates).map(ItemCandidate::attributes).allSatisfy(attrs -> assertThat(attrs).containsKeys(
-                // top level attributes
-                "drupal_internal__nid", "drupal_internal__vid", "langcode", "revision_timestamp", "created", "changed"
-                , "path", "body"));
+        assertThat(candidates).map(ItemCandidate::attributes)
+                .allSatisfy(attrs -> assertThat(attrs).containsKeys(
+                        // top level attributes
+                        "drupal_internal__nid", "drupal_internal__vid", "langcode", "revision_timestamp",
+                        "created", "changed", "path", "body",
+                        // from related nodes, connected with a dot
+                        "revision_uid.id",  // do we need to put this?
+                        "revision_uid.display_name",
+                        "uid.id",
+                        "uid.display_name",
+                        "node_type.id",  // not resolved in includes, still put top level relationship's ID?
+                        "field_media_image.id",
+                        "field_media_image.drupal_internal_mid",
+                        "field_media_image.path",
+                        "field_media_image.created",
+                        "field_media_image.changed",
+
+                        // nested relationship
+                        "field_media_image.bundle.id",
+                        "field_media_image.bundle.label"
+                ));
 
         assertThat(candidates.get(0).attributes()).containsAllEntriesOf(Map.of(
                 "promote", true,
                 "sticky", false,
                 "moderation_state", "published"
         ));
+        assertThat(candidates.get(0).attributes()).containsAllEntriesOf(Map.of(
+                "revision_uid.id", UUID.fromString("3a577e34-2a6a-4ed1-acb3-de923de6a313"),
+                "revision_uid.display_name", "Umami",
+                "uid.display_name", "Umami",
+
+                "field_media_image.id", UUID.fromString("dd4fe495-ef6d-47ae-a056-d5a2f4bb6d25"),
+                "field_media_image.langcode", "en",
+                "field_media_image.revision_created", ZonedDateTime.parse("2025-01-08T12:56:13+00:00"),
+                "field_media_image.content_translation_source", "und",
+                "field_media_image.content_translation_outdated", false,
+
+                // nested relationship
+                "field_media_image.bundle.id", UUID.fromString("7857f44f-ae5b-42cf-b35d-cd7d900423eb"),
+                "field_media_image.bundle.label", "Image"
+        ));
+
+        assertThat(candidates.get(0).attributes().get("field_media_image.path"))
+                .extracting("alias", "pid", "langcode")
+                .containsExactly(null, null, "en");
     }
 }
